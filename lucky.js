dc8460e871c5c4494af16ce36a0c498d581057e3
Hoseok Kim <hskim@wemakeprice.com>
2016-10-14 14:27:47 +0900

    띠별 운세 업데이트

---
+++ lucky.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+var cheerio = require("cheerio");
+var moment = require("moment");
+
+var url = "http://www.unsin.co.kr/unse/free/todayline/result";
+
+var oneLineArr = new Array();
+var yearsArr = new Array();
+var descriptionArr = new Array();
+
+request(url, function(error, response, body) {
+  if (error) throw error;
+
+  var $ = cheerio.load(body);
+  var oneLine = $("div.title dd");          // 한줄 요약
+  var years = $("div.year tbody th");       // 년도
+  var description = $("div.year tbody td"); // 운세 내용
+
+  // 띠별 한줄 운세 요약
+  oneLine.each(function(){
+      oneLineArr.push($(this).text());
+  });
+
+  // 년도
+  years.each(function(){
+      yearsArr.push($(this).text());
+  });
+
+  // 운세 내용
+  description.each(function(){
+      descriptionArr.push($(this).text());
+  });
+});
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res){
+    res.render("lucky", {oneLineTxt:oneLineArr, yearsTxt:yearsArr, descriptionTxt:descriptionArr, moment:moment});
+});
+
+app.listen(9000, function(){
+   console.log('9000 port Server On!');
+});
